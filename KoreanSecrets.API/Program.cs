0f28cace205f54c8038bbf6fc04da4484afc74d1
Yaroslav Terekh <yarolslavterekh@gmail.com>
2023-08-22 16:05:48 +0300

    Added database connection

--- KoreanSecrets.API/Program.cs
+++ KoreanSecrets.API/Program.cs
@@ -1,6 +1,10 @@
+using KoreanSecrets.Domain.DbConnection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContextsCustom(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ KoreanSecrets.Domain/DbConnection/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoreanSecrets.Domain.DbConnection;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> opts) : base(opts) { }
+}
