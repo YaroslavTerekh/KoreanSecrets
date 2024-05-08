5af43077f58cbc7edea7fc1434ba499e5b54006d
Roma Yezerskyi <yezerskyi.roman@gmail.com>
2024-05-08 15:12:34 +0300

    fix

--- KoreanSecrets.BL/Behaviors/Admin/Products/GetPurchases/GetPurchasesHandler.cs
+++ KoreanSecrets.BL/Behaviors/Admin/Products/GetPurchases/GetPurchasesHandler.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using KoreanSecrets.Domain.Common.Enums;
 
 namespace KoreanSecrets.BL.Behaviors.Admin.Products.GetPurchases;
 
@@ -30,7 +31,15 @@ public class GetPurchasesHandler : IRequestHandler<GetPurchasesQuery, Pagination
 
         if(request.Status is not null)
         {
-            purchases = purchases.Where(t => t.PurchaseStatus == request.Status);
+            if (request.Status == PurchaseStatus.New || request.Status == PurchaseStatus.Waiting)
+            {
+                purchases = purchases.Where(t => (t.PurchaseStatus == PurchaseStatus.New || t.PurchaseStatus == PurchaseStatus.Waiting));
+            }
+            
+            if (request.Status == PurchaseStatus.Success || request.Status == PurchaseStatus.Failure)
+            {
+                purchases = purchases.Where(t => (t.PurchaseStatus == PurchaseStatus.Success || t.PurchaseStatus == PurchaseStatus.Failure));
+            }
         }
 
         var purchasesEntities = await purchases
