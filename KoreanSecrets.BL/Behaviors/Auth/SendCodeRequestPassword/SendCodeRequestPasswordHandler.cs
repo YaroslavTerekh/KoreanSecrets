406913d3610127f8b3674dffb162f9b1aebdf96c
Yaroslav Terekh <yarolslavterekh@gmail.com>
2024-06-01 20:54:08 +0300

    fix

--- KoreanSecrets.BL/Behaviors/Auth/SendCodeRequestPassword/SendCodeRequestPasswordHandler.cs
+++ KoreanSecrets.BL/Behaviors/Auth/SendCodeRequestPassword/SendCodeRequestPasswordHandler.cs
@@ -28,7 +28,7 @@ public class SendCodeRequestPasswordHandler : IRequestHandler<SendCodeRequestPas
 
     public async Task<Unit> Handle(SendCodeRequestPasswordCommand request, CancellationToken cancellationToken)
     {
-        var user = await _context.Users.FirstOrDefaultAsync(t => t.PhoneNumber == request.PhoneNumber, cancellationToken);
+        var user = await _context.Users.FirstOrDefaultAsync(t => t.PhoneNumber == _phoneNumberService.FormatPhoneNumber(request.PhoneNumber), cancellationToken);
 
         if (user is null)
             throw new NotFoundException(ErrorMessages.UserNotFound);
@@ -45,7 +45,7 @@ public class SendCodeRequestPasswordHandler : IRequestHandler<SendCodeRequestPas
         var message = await MessageResource.CreateAsync(
             body: ValidationMessages.VerificationCodeInfo(user.TemporaryCode),
             from: new PhoneNumber(_twilioSettings.FromPhoneNumber),
-            to: new PhoneNumber(user.PhoneNumber)
+            to: new PhoneNumber(_phoneNumberService.FormatPhoneNumber(request.PhoneNumber))
         );
 
         return Unit.Value;
