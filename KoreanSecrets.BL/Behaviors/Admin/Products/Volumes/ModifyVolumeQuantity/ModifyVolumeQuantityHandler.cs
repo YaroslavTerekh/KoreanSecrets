7811e6588a4fc98d460cca6e86d2cf548328e811
Yaroslav Terekh <yarolslavterekh@gmail.com>
2024-07-21 21:52:48 +0300

    fix

--- KoreanSecrets.BL/Behaviors/Admin/Products/Volumes/ModifyVolumeQuantity/ModifyVolumeQuantityHandler.cs
+++ KoreanSecrets.BL/Behaviors/Admin/Products/Volumes/ModifyVolumeQuantity/ModifyVolumeQuantityHandler.cs
@@ -1,9 +1,14 @@
 ﻿using KoreanSecrets.BL.Services.Abstractions;
+using KoreanSecrets.BL.Services.Realizations;
 using KoreanSecrets.Domain.Common.Constants;
+using KoreanSecrets.Domain.Common.Settings;
 using KoreanSecrets.Domain.DbConnection;
 using KoreanSecrets.Domain.Models;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
 
 namespace KoreanSecrets.BL.Behaviors.Admin.Products.Volumes.ModifyVolumeQuantity;
 
@@ -11,11 +16,17 @@ public class ModifyVolumeQuantityHandler : IRequestHandler<ModifyVolumeQuantityC
 {
     private readonly DataContext _context;
     private readonly IEmailService _emailService;
+    private readonly IConfiguration _config;
+    private readonly IPhoneNumberService _phoneNumberService;
+    private readonly TwillioSettings _twilioSettings;
 
-    public ModifyVolumeQuantityHandler(DataContext context, IEmailService emailService)
+    public ModifyVolumeQuantityHandler(DataContext context, IEmailService emailService, IConfiguration config, IPhoneNumberService phoneNumberService, TwillioSettings twillioSettings)
     {
         _context = context;
         _emailService = emailService;
+        _config = config;
+        _twilioSettings = twillioSettings;
+        _phoneNumberService = phoneNumberService;
     }
 
     public async Task<Unit> Handle(ModifyVolumeQuantityCommand request, CancellationToken cancellationToken)
@@ -25,20 +36,29 @@ public class ModifyVolumeQuantityHandler : IRequestHandler<ModifyVolumeQuantityC
             .FirstOrDefaultAsync(t => t.Id == request.Id, cancellationToken);
 
         if (volume is null) throw new Exception(ErrorMessages.ProductNotFound("Об'єкту об'єму для модифікації"));
-        
-        if(volume.Quantity == 0 && request.Quantity > 0)
+
+        if (volume.Quantity == 0 && request.Quantity > 0)
         {
             try
             {
                 if (volume.IsInStock)
                 {
-                    var message = new Message(volume.UsersWaitingForStock.Select(t => t.User.Email).ToArray(), "Товар в наявності!", volume.Product.Title);
+                    var message = new Message(volume.UsersWaitingForStock.Select(t => t.User.Email).ToArray(),
+                        "Товар в наявності!", String.Concat("Товар", volume.Product.Title, "з'явився у наявності!", _config.GetSection("HostSettings:FrontApplicationUrl"), "home/item/", volume.Product.Id));
 
                     await _emailService.SendEmailAsync(message, "Товар в наявності");
 
                     volume.UsersWaitingForStock.Clear();
 
                     await _context.SaveChangesAsync(cancellationToken);
+
+                    foreach (var volumeUser in volume.UsersWaitingForStock)
+                    {
+                        await SendMessage($"Secrets of care | Товар {volume.Product.Title} у наявності!",
+                            volumeUser.User.PhoneNumber);
+                        await SendMessage($"https://www.secretsofcare.com.ua/home/purchase/{volumeUser.Volume.ProductId}",
+                            volumeUser.User.PhoneNumber);
+                    }
                 }
             }
             catch (Exception e)
@@ -52,5 +72,20 @@ public class ModifyVolumeQuantityHandler : IRequestHandler<ModifyVolumeQuantityC
         await _context.SaveChangesAsync(cancellationToken);
 
         return Unit.Value;
+    }
+
+    private async Task SendMessage(string text, string phone)
+    {
+        try
+        {
+            await MessageResource.CreateAsync(
+            body: text,
+                from: new PhoneNumber(_twilioSettings.FromPhoneNumber),
+                to: new PhoneNumber(_phoneNumberService.FormatPhoneNumber(phone))
+            );
+        }
+        catch (Exception e)
+        {
+        }
     }
 }
